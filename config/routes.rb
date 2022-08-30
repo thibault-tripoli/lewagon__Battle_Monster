a16ead7c3d506b61b2733825171e501c795227ee
cardon <philantrope1@outlook.com>
2022-08-30 17:51:38 +0200

    fix log_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  #get '/users/sign_out' => 'devise/sessions#destroy'
   root to: "pages#home"
   resources :battles, except: :index
   get 'profil/:id', to: 'pages#profil', as: :profil
