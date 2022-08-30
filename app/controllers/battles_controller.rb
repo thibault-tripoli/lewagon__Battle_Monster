3c2e1dfc925c63823c96feed1ee5f4b2f61eef68
thibault tripoli <thibault@dropagence.fr>
2022-08-30 18:08:08 +0200

    start combats

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,4 +1,12 @@
 class BattlesController < ApplicationController
+  def show
+    @battle = Battle.find(1)
+    @deck1 = Deck.create(user_id: 1, monster_id: 1, battle_id: @battle.id, attack_id: 1, hp: 100)
+    @deck2 = Deck.create(user_id: 2, monster_id: 2, battle_id: @battle.id, attack_id: 2, hp: 100)
+    @battle.current_deck_id = @deck1.id
+    @battle.save
+  end
+
   def new
   end
 
@@ -12,8 +20,5 @@ class BattlesController < ApplicationController
   end
 
   def edit
-  end
-
-  def show
   end
 end
