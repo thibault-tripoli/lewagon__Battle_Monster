526a192c8aedca344e65daae059e78282485c217
cardon <philantrope1@outlook.com>
2022-08-30 16:40:50 +0200

    creation controller battles, routes et views

---
+++ app/controllers/battles_controller.rb
@@ -0,0 +1,19 @@
+class BattlesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
