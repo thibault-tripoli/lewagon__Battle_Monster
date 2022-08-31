b98c870e2f552310bdf4d2201c9630c4de802a34
John78468 <73955607+John78468@users.noreply.github.com>
2022-08-31 11:58:19 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,17 @@ Rails.application.routes.draw do
   resources :battles, except: :index
   get 'profil/:id', to: 'pages#profil', as: :profil
 
+
+
+
+
+
+
+
+
+
+
+
   get "page/main", to: 'pages#main'
   get "page/start_intro", to: 'pages#start_intro'
   get "page/start_monster", to: 'pages#start_monster'
