af476d83be2e632065c197ac3f3b71785f5ce002
thibault tripoli <thibault@dropagence.fr>
2022-09-01 22:08:49 +0200

    animations finies

--- app/javascript/controllers/battle_subscription_controller.js
+++ app/javascript/controllers/battle_subscription_controller.js
@@ -19,43 +19,67 @@ export default class extends Controller {
       { received: data => {
           const json = JSON.parse(data)
           this.roundsTarget.innerHTML = json.html
+          const roundControl = document.querySelector('#round-controls')
+
           const monster1 = document.querySelector('#arena-deck-1-monster')
           const monster2 = document.querySelector('#arena-deck-2-monster')
-          const roundControl = document.querySelector('#round-controls')
+          const arenaAttack1 = document.querySelector('#arena-attack-1')
+          const arenaAttack2 = document.querySelector('#arena-attack-2')
 
           if (json.attack == 1) {
 
-            const arenaAttack1 = document.querySelector('#arena-attack-1')
             // start
             monster1.classList.add('attack')
             roundControl.classList.add('controls-off')
             // cascade
             monster1.addEventListener("animationend", function(){
-              monster1.classList.remove('attack')
-              arenaAttack1.classList.add('attack-in')
+              arenaAttack1.classList.add('attack-1')
             });
 
             arenaAttack1.addEventListener("animationend", function(){
-              arenaAttack1.classList.remove('attack-in')
               monster2.classList.add('hurt')
             });
 
             monster2.addEventListener("animationend", function(){
-              monster2.classList.remove('hurt')
               document.querySelector('.prog-2').style.width="" + json.deck2_HP + "%"
-              roundControl.classList.remove('controls-off')
             });
 
-            // titre.addEventListener("animationend", goToHurt);
-            // monster.addEventListener("animationend", goToHp);
+            document.querySelector('#progress2').addEventListener("transitionend", function(){
+              roundControl.classList.remove('controls-off')
+              arenaAttack1.classList.remove('attack-1')
+              monster2.classList.remove('hurt')
+              monster1.classList.remove('attack')
+              arenaAttack2.classList.remove('attack-2')
+              monster1.classList.remove('hurt')
+              monster2.classList.remove('attack')
+            });
 
-            // document.querySelector('#arena-deck-1-monster').style.fontSize="50px"
-            // document.querySelector('#arena-deck-2-monster').style.fontSize="20px"
-            // document.querySelector('.prog-2').style.width="" + json.deck2_HP + "%"
           } else if (json.attack == 2) {
-            document.querySelector('#arena-deck-1-monster').style.fontSize="20px"
-            document.querySelector('#arena-deck-2-monster').style.fontSize="50px"
-            document.querySelector('.prog-1').style.width="" + json.deck1_HP + "%"
+
+            monster2.classList.add('attack')
+            roundControl.classList.add('controls-off')
+            // cascade
+            monster2.addEventListener("animationend", function(){
+              arenaAttack2.classList.add('attack-2')
+            });
+
+            arenaAttack2.addEventListener("animationend", function(){
+              monster1.classList.add('hurt')
+            });
+
+            monster1.addEventListener("animationend", function(){
+              document.querySelector('.prog-1').style.width="" + json.deck1_HP + "%"
+            });
+
+            document.querySelector('#progress1').addEventListener("transitionend", function(){
+              roundControl.classList.remove('controls-off')
+              arenaAttack2.classList.remove('attack-2')
+              monster1.classList.remove('hurt')
+              monster2.classList.remove('attack')
+              arenaAttack1.classList.remove('attack-1')
+              monster2.classList.remove('hurt')
+              monster1.classList.remove('attack')
+            });
 
           }
         }
