34172b8bb310d33c7adf6282250f77c9f5bd0f2d
thibault tripoli <thibault@dropagence.fr>
2022-09-05 11:44:16 +0200

    maj routes

---
+++ config/routes.rb
@@ -0,0 +1,32 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: "pages#home"
+
+  # Battles
+  resources :battles, only: %i[show create update] do
+    get 'next_round/:id', to: 'battles#next_round', as: :next_round
+    get "setup", to: 'battles#setup', as: :setup
+    get "loading", to: 'battles#loading', as: :loading
+  end
+  get "match", to: 'battles#match', as: :match
+
+  # Monsters
+  resources :monsters, only: :create
+
+  # Pages
+  get "page/main", to: 'pages#main'
+  get "page/start_intro", to: 'pages#start_intro'
+  get "page/start_monster", to: 'pages#start_monster'
+  get "page/start_tutoriel", to: 'pages#start_tutoriel'
+  get "page/combats", to: "pages#combats"
+  get "page/fake", to: "pages#fake"
+
+  # Pages : Profil
+  get 'profil/:id', to: 'pages#profil', as: :profil
+  get 'profil/:id/edit', to: 'pages#edit'
+
+  # Templates
+  get 'templates/container'
+  get 'templates/main'
+  get 'templates/master'
+end
