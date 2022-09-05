cfff0266b2162fd56556d73befbf039beb5a15f6
cardon <philantrope1@outlook.com>
2022-09-05 11:31:51 +0200

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
-  validates :name, presence: :true
+  validates :name, presence: true
 end
