83ed844d36c78f02851d240d925b63ce8fb474d1
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2022-09-06 16:26:13 +0200

    added css

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -8,6 +8,7 @@ class MonstersController < ApplicationController
     @monster.user = current_user
     @monster.xp = 0
     if @monster.save!
+      current_user.onboard = true
       redirect_to page_start_tutoriel_path
     else
       render :start_monster, status: :unprocessable_entity
